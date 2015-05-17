068c6589e7d7733ec2717d68ae67f7dd64eefe02
Corey Ford <corey@coreyford.name>
2015-05-17 16:11:25 -0700

    Add fake data

--- trash/device.py
+++ trash/device.py
@@ -1,6 +1,15 @@
 import requests
 import config
 
+fake_data = [
+    {'lat': 35.27876, 'lon': -120.65777, 'distance': 100, 'name': 'Mitchell'},
+    {'lat': 35.28037, 'lon': -120.66333, 'distance': 100, 'name': 'Mission'},
+    {'lat': 35.26796, 'lon': -120.64693, 'distance': 100, 'name': 'Sinsheimer'},
+    {'lat': 35.26879, 'lon': -120.65926, 'distance': 100, 'name': 'Meadow'},
+    {'lat': 35.26301, 'lon': -120.68394, 'distance': 100, 'name': 'Laguna'},
+    {'lat': 35.28997, 'lon': -120.66535, 'distance': 100, 'name': 'Santa Rosa'}
+]
+
 def get_resources(server, auth, domain, wanted):
     endpoints = []
     url = '{}/{}/endpoints'.format(server, domain)
@@ -25,5 +34,5 @@ def trashcans():
         '/3302/0/5600': 'distance'
     }
 
-    return get_resources(server, auth, domain, wanted)
+    return get_resources(server, auth, domain, wanted) + fake_data
 
