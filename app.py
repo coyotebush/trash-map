22037c81e4b796c5ce84b7f56743584043b0b774
Corey Ford <corey@coreyford.name>
2015-05-16 18:24:21 -0700

    Show data over Mapbox tiles

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, jsonify
-from trash import device
+from trash import device, config
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def map():
-    return render_template('map.html')
+    return render_template(
+        'map.html',
+        mapbox_id=config.MAPBOX_ID,
+        mapbox_token=config.MAPBOX_TOKEN
+    )
 
 @app.route('/trashcans')
 def trashcans():
