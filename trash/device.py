18c1f699f373945ea989411a2deeabb221f46162
Corey Ford <corey@coreyford.name>
2015-05-17 17:08:50 -0700

    Use reported units

--- trash/device.py
+++ trash/device.py
@@ -2,12 +2,12 @@ import requests
 import config
 
 fake_data = [
-    {'lat': 35.27876, 'lon': -120.65777, 'distance': 100, 'name': 'Mitchell'},
-    {'lat': 35.28037, 'lon': -120.66333, 'distance': 100, 'name': 'Mission'},
-    {'lat': 35.26796, 'lon': -120.64693, 'distance': 100, 'name': 'Sinsheimer'},
-    {'lat': 35.26879, 'lon': -120.65926, 'distance': 100, 'name': 'Meadow'},
-    {'lat': 35.26301, 'lon': -120.68394, 'distance': 100, 'name': 'Laguna'},
-    {'lat': 35.28997, 'lon': -120.66535, 'distance': 100, 'name': 'Santa Rosa'}
+    {'lat': 35.27876, 'lon': -120.65777, 'distance': 1, 'units': 'm', 'name': 'Mitchell'},
+    {'lat': 35.28037, 'lon': -120.66333, 'distance': 1, 'units': 'm', 'name': 'Mission'},
+    {'lat': 35.26796, 'lon': -120.64693, 'distance': 1, 'units': 'm', 'name': 'Sinsheimer'},
+    {'lat': 35.26879, 'lon': -120.65926, 'distance': 1, 'units': 'm', 'name': 'Meadow'},
+    {'lat': 35.26301, 'lon': -120.68394, 'distance': 1, 'units': 'm', 'name': 'Laguna'},
+    {'lat': 35.28997, 'lon': -120.66535, 'distance': 1, 'units': 'm', 'name': 'Santa Rosa'}
 ]
 
 def get_resources(server, auth, domain, wanted):
@@ -31,7 +31,8 @@ def trashcans():
     wanted = {
         '/6/0/0': 'lat',
         '/6/0/1': 'lon',
-        '/3302/0/5600': 'distance'
+        '/3302/0/5700': 'distance',
+        '/3302/0/5701': 'units'
     }
 
     return get_resources(server, auth, domain, wanted) + fake_data
