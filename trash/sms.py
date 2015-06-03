08be51b420a75acc1612a32a17c2e9fe7ca1ceeb
Corey Ford <corey@coreyford.name>
2015-06-03 17:49:00 +0000

    SMS to multiple recipients

--- trash/sms.py
+++ trash/sms.py
@@ -1,6 +1,7 @@
 from twilio.rest import TwilioRestClient
 import config
 
-def send(body, to=config.TWILIO_RECIPIENT):
+def send(body, recipients=config.TWILIO_RECIPIENTS):
     client = TwilioRestClient(config.TWILIO_ACCOUNT, config.TWILIO_TOKEN)
-    client.messages.create(to=to, from_=config.TWILIO_FROM, body=body)
+    for to in recipients:
+        client.messages.create(to=to, from_=config.TWILIO_FROM, body=body)
